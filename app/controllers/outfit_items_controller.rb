8c360905beac90b784578e7f9e732e8dd084dbc6
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2023-12-06 18:56:52 +0000

    deleting all commented out code so Ian doesn't hate me

--- app/controllers/outfit_items_controller.rb
+++ app/controllers/outfit_items_controller.rb
@@ -1,25 +1,20 @@
 class OutfitItemsController < ApplicationController
   before_action :set_outfit_item, only: %i[ show edit update destroy ]
 
-  # GET /outfit_items or /outfit_items.json
   def index
     @outfit_items = OutfitItem.all
   end
 
-  # GET /outfit_items/1 or /outfit_items/1.json
   def show
   end
 
-  # GET /outfit_items/new
   def new
     @outfit_item = OutfitItem.new
   end
 
-  # GET /outfit_items/1/edit
   def edit
   end
 
-  # POST /outfit_items or /outfit_items.json
   def create
     @outfit_item = OutfitItem.new(outfit_item_params)
 
@@ -34,7 +29,6 @@ class OutfitItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /outfit_items/1 or /outfit_items/1.json
   def update
     respond_to do |format|
       if @outfit_item.update(outfit_item_params)
@@ -47,7 +41,6 @@ class OutfitItemsController < ApplicationController
     end
   end
 
-  # DELETE /outfit_items/1 or /outfit_items/1.json
   def destroy
     @outfit_item.destroy
 
@@ -58,12 +51,10 @@ class OutfitItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_outfit_item
       @outfit_item = OutfitItem.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def outfit_item_params
       params.require(:outfit_item).permit(:outfit_id, :item_id)
     end
