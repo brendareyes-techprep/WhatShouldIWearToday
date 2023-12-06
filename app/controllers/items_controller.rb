54f319bddb94fc1248a168aee7c1a039fe0f2fb1
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2023-12-06 18:25:58 +0000

    so far not getting any errors with pundit, just fixed the links on the navbar so that they follow the link_to format

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def index
