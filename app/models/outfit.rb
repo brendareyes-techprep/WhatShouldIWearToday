74bc2c998ae0a764f54023c8e8687cedcfe1b9be
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2023-12-12 20:39:25 +0000

    fixing schema

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -3,7 +3,6 @@
 # Table name: outfits
 #
 #  id         :integer          not null, primary key
-#  date_made  :date
 #  vibe       :url
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
